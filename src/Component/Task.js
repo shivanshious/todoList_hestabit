4a6f6206a45f05d7f80a10a3c2743b067a618c47
shivanshious <shivanshgoel98@gmail.com>
2022-10-10 19:51:36 +0530

    commit on 10 oct

--- src/Component/Task.js
+++ src/Component/Task.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import Style from "../Styles/task.module.css";
 import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
 import DeleteIcon from "@mui/icons-material/Delete";
@@ -8,20 +8,24 @@ import { GlobalContext } from "./GlobalProvider";
 const Task = (props) => {
   const { task } = props;
   const { deleteTaskFromList } = useContext(GlobalContext);
+  const [isCompleted,setIsCompleted]=useState(false)
 
   const handleDelete = () => {
     deleteTaskFromList(task);
   };
+  const changeStatus=()=>{
+    setIsCompleted(!isCompleted)
+  }
   return (
     <>
       <div className={`${Style.task_container}`}>
         <div className={`${Style.task}`}>
-          <h3 className={`${Style.main_task}`}>{task}</h3>
+          <h3 className={isCompleted===true?`${Style.completed_checked}`:`${Style.main_task}`}>{task}</h3>
         </div>
         <div className={`${Style.task_actions}`}>
           <EditIcon className={`${Style.edit}`} />
           <DeleteIcon className={`${Style.delete}`} onClick={handleDelete} />
-          <CheckCircleOutlineIcon className={`${Style.completed}`} />
+          <CheckCircleOutlineIcon className={`${Style.completed}`} onClick={changeStatus}/>
         </div>
       </div>
     </>
