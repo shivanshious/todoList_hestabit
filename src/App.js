45d2ac17eac27bd57ba5241ca3041e5298c98106
shivanshious <shivanshgoel98@gmail.com>
2022-10-13 15:32:13 +0530

    commit on 13 oct

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import GlobalProvider from "./Component/GlobalProvider";
 function App() {
   return (
     <>
+      <h1 className="heading">ToDo List</h1>
       <div className="main">
         <GlobalProvider>
           <Content />
